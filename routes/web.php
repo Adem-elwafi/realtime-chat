d96d3e943a8285711973ffc3730156c2ed8bba70
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-01-06 20:52:26 +0100

    feat(routes): add home and dashboard routes with auth protection

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,35 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Home route - redirects based on authentication status
 Route::get('/', function () {
-    return view('welcome');
-});
+    // If user is authenticated, redirect to dashboard
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
+    
+    // Otherwise, redirect to login page
+    return redirect()->route('login');
+})->name('home');
 
+// Dashboard route - protected by auth middleware
+// Only authenticated users can access this route
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
+// Include authentication routes from Breeze
 require __DIR__.'/auth.php';
