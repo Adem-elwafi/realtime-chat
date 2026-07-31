237985a75dd7e86b383608f32e6868ef5609c4b2
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-07-31 18:49:43 +0100

    fix(cors): allow blog-platform origin localhost:8001

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8001'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
